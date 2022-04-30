fa29e16d9c35c33891cf0e99a430eb126b4d49d4
shahZaheed <zahidshah99599@gmail.com>
2022-04-30 16:56:44 +0530

    ububtu

--- traffic/lib/main.dart
+++ traffic/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:traffic/screens/new_root.dart';
+
 import 'package:traffic/screens/root_screen.dart';
 
 Future<void> main() async {
@@ -20,6 +20,6 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const NewRootScreen());
+        home: const RootScreen());
   }
 }
